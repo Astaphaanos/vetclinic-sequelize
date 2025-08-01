19aa49dc64d6169259ae22c6b175a1550fadf37b
Astaphaanos <anclar206@outlook.com>
2025-08-01 11:00:41 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 const User = require('./models/User')
 const Vet = require('./models/Vet')
+const { where } = require('sequelize')
 
 const app = express()
 
@@ -23,25 +24,37 @@ app.use(express.static('public'))
 
 //* POST INSERT pet
 app.post('/users/create', async(req, res) => {
-    const {nome, especie, raca, idade, sexo, nomeDoTutor} = req.body
+    const {nome, especie, raca, idade, sexo, nomeDoTutor, VetId} = req.body
 
     console.log(req.body)
 
-    await User.create({nome, especie, raca, idade, sexo, nomeDoTutor})
-    res.redirect('/')
+    try {
+        await User.create({nome, especie, raca, idade, sexo, nomeDoTutor, VetId})
+        res.redirect('/')
+    } catch(err) {
+        console.log('Erro ao criar o pet:', err)
+    }
 })
 
 //* INSERT pet
-app.get('/users/create', (req,res) => {
-    res.render('insertpet')
+app.get('/users/create', async(req,res) => {
+    try{
+        const vet = await Vet.findAll({raw:true})
+        res.render('insertpet', {vet: vet})
+    } catch(err) {
+        console.log('Erro ao buscar veterinário', err)
+    }
 })
 
 //* WHERE (página individual de cada paciente pet)
 app.get('/users/:id', async(req, res) => {
     const id = req.params.id
 
-    const user = await User.findOne({raw: true, where: {id:id} })
-    res.render('petview', {user})
+    const user = await User.findOne({include: Vet, where: {id:id} })
+
+    console.log(user.get({ plain: true })); // Veja se o Vet aparece aqui
+    
+    res.render('petview', {user: user.get({ plain: true })})
 })
 
 //* DELETE
@@ -55,14 +68,21 @@ app.post('/users/delete/:id', async(req,res) => {
 //* UPDATE 
 app.get('/users/update/:id', async(req,res) => {
     const id = req.params.id
+    const vet = await Vet.findAll()
 
-    const user = await User.findOne({ raw: true, where: {id:id} })
+    try {
+        const user = await User.findOne({ include: Vet, where: {id:id} })
 
-    res.render('updatepet', {user})
+        res.render('updatepet', {user: user.get({ plain: true }), 
+            vet: vet.map(vet => vet.get({plain:true}))
+        })
+    } catch(error) {
+        console.log(error)
+    }
 })
 
 app.post('/users/update', async(req,res) => {
-    const {id, name, especie, raca, idade, sexo, nomeDoTutor} = req.body
+    const {id, name, especie, raca, idade, sexo, nomeDoTutor, VetId} = req.body
 
     const userData = {
         id,
@@ -72,6 +92,7 @@ app.post('/users/update', async(req,res) => {
         idade,
         sexo,
         nomeDoTutor,
+        VetId
     }
 
     await User.update(userData, {where: {id:id} })
@@ -80,16 +101,26 @@ app.post('/users/update', async(req,res) => {
 
 //* HOME PAGE
 app.get('/', async(req,res) => {
-    const users = await User.findAll({raw: true})
-    
-    res.render('home', {users: users})
+    try {
+        const users = await User.findAll({include: Vet})
+
+        res.render('home', {users: users.map(user => user.get({plain: true}))})
+    } catch(err) {
+        console.log('Erro ao buscar pacientes:', err)
+    }
 })
 
 //* VET TABLE
 
 //! Renderizando os veterinários
 app.get('/vet/vetview', async(req,res) => {
-    res.render('vetview')
+    try {
+        const vet = await Vet.findAll({raw:true})
+        res.render('vetview', {vet: vet})
+    } catch(err) {
+        console.log('Erro ao buscar veterinários:', err)
+        res.status(500).send('Erro ao buscar veterinários')
+    }
 })
 
 //! Renderizando o formulários para registar o vet
@@ -107,7 +138,20 @@ app.post('/vet/create', async(req,res) => {
 
     await Vet.create(vet)
 
-    res.redirect('/')
+    res.redirect('/vet/vetview')
+})
+
+//* DELETE VET
+app.post('/vet/delete/:id', async (req,res) => {
+    const id = req.params.id
+
+    try {
+        await Vet.destroy({ where: {id:id} })
+        res.redirect('/vet/vetview')
+    } catch(err) {
+        console.log('Erro ao excluir veterinário', err)
+    }
+
 })
 
 conn.sync().then(() => {  
